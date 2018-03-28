623bc52ca8cae2c7a18230fa1f7ab6fe793e6dca
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 01:30:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,4 +100,14 @@ class Student
 
     DB[:conn].execute(sql).map{ |row| self.new_from_db(row) }.first
   end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map{ |row| self.new_from_db(row) }
+  end
 end
