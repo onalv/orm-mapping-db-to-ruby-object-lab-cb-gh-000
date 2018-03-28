ee22bf73591ce549935c5d10656d19a22308fe13
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 01:26:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,11 +61,11 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT * 
+      SELECT *
       FROM students
       WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql).map{ |row| self.new_from_db(row) }.first
+    DB[:conn].execute(sql).map{ |row| self.new_from_db(row) }
   end
 end
